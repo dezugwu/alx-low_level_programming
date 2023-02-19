b15688bc7f42010b49d0d84b9f5eb8b6565e27d0
dezugwu <desmond.ezugwu@gmail.com>
2023-02-19 10:31:33 -0800

    Application of printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print " ith proper grammar, but the outcome is a piece of art," followed by newline
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
