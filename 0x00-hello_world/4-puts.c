fe4f5df12d4500349bfc5535efaf3995598f6a51
dezugwu <desmond.ezugwu@gmail.com>
2023-02-19 08:28:40 -0800

    How to Use the function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main -Print "Programming is like building a multilingual puzzle"
+ *
+ * return 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
+
