18cf59aa90a244d13873947ed6f2772fb3f0a706
dezugwu <desmond.ezugwu@gmail.com>
2023-02-21 20:23:15 -0800

    7-print_last_digit.c mistake corrected

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,17 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
 
-	if (ld < 0)
-		ld *= -1;
+	int last;
 
-	_putchar(ld + '0');
+	last = n % 10;
 
-	return (0);
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+
+	_putchar(last + '0');
+
+	return (last);
 }
